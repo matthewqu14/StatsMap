93833024b2972bdac29cc6d3c19a6eaa132a03cf
matthewqu14 <matthewqu@college.harvard.edu>
2020-02-09 16:13:08 -0500

    Added text popup to show raw # of matches

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ var svg = d3.select("svg"),
     height = +svg.attr("height");
 
 // Map and projection
-// World map use this
+// Change scale and translate to move to correct place
 var projection = d3.geoNaturalEarth1()
     .scale(800)
-    .translate([1920,980]);
+    .translate([2000,1000]);
 
-// US map use another projection
+// US map use another projection maybe (GEOAlbersUSA?)
 
 // A path generator
 var path = d3.geoPath()
@@ -26,6 +26,7 @@ d3.queue()
 function ready(error, dataGeo, data, top10) {
 
     // Reformat the list of link.
+    // Draws all lines between every state
     var link = [];
     i = 0;
     data.forEach(function (row1) {
@@ -59,7 +60,7 @@ function ready(error, dataGeo, data, top10) {
         .style("stroke", "#fff")
         .style("stroke-width", 0.3);
 
-    // Add the path
+    // Add the paths
     svg.selectAll("myPath")
         .data(link)
         .enter()
@@ -73,33 +74,74 @@ function ready(error, dataGeo, data, top10) {
         .attr("pointer-events", "none")
         .style("fill", "none")
         .style("stroke", "#ffdbea")
+        // Makes paths invisible
         .style("stroke-width", 0);
+
+    var place = svg.selectAll("g")
+        .data(data)
+        .enter()
+        .append("g")
+        .attr("transform", function(d) {
+            return "translate(" + projection([+d.long, +d.lat])[0] + "," + (projection([+d.long, +d.lat])[1] - 10) + ")"
+        })
+        .attr("id", function (d) {
+            return "place" + d.state;
+        })
+        .append("text")
+        .text("")
+        .attr("id", function(d) {
+            return "text" + d.state;
+        })
+        .attr("text-anchor", "middle")
+        .attr("fill", "black")
+        .attr("font-family", "Verdana")
+        .attr("font-size", "11px")
+        .attr("display", "none");
 
     var paths = document.getElementsByClassName("line");
     top10array = [];
     percentarray = [];
+    numarray = [];
 
+    // Script to make lines visible on hover functionality
     $(document).ready(function () {
         $('.state').hover(function () {
+            // On hover over state do all of this (chooses paths to draw and text to show)
             statename = this.id.toString();
             top10.forEach(function (row) {
                 if (statename === row.state) {
                     top10array = row.top10;
                     percentarray = row.percent;
-                    console.log(top10array);
+                    numarray = row.nummatches;
                 }
             });
             for (i = 0; i < paths.length; i++) {
                 for (j = 0; j < 10; j++) {
-                    if (paths[i].classList.contains(top10array[j]) && paths[i].classList.contains(statename) && top10array[j] !== statename) {
-                        paths[i].style.strokeWidth = percentarray[j]/2.0; //add some scale to make min and max line widths good
+                    if (top10array[j] === statename) {
+                        continue;
+                    }
+                    else if (paths[i].classList.contains(top10array[j]) && paths[i].classList.contains(statename)) {
+                        // The scale right now is just divide percent by 2 which might not be suitable
+                        // Depending on the data, make the scale probably have min strokeWidth 0.5 or 1 and max around 4
+                        paths[i].style.strokeWidth = percentarray[j]/2.0;
                     }
                 }
             }
+            for (j = 0; j < 10; j++) {
+                text = document.getElementById("text" + top10array[j]);
+                text.style.display = "inline";
+                text.textContent = ("Matches to " + top10array[j] + ": " + numarray[j])
+                    .split("_").join(" ");
+            }
         },
+            // On hover off state do this (hides text and paths)
         function () {
             for (i = 0; i < paths.length; i++) {
                 paths[i].style.strokeWidth = 0;
+            }
+            for (j = 0; j < 10; j++) {
+                text = document.getElementById("text" + top10array[j]);
+                text.style.display = "none";
             }
         });
     })
